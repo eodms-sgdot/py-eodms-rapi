2b765e79d3ce719613e3fb0dd9dcef9097817aae
Kevin Ballantyne <kevin.ballantyne@canada.ca>
2021-08-16 10:29:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,21 @@ setup(
     include_package_data=True, 
     url='https://github.com/nrcan-eodms-sgdot-rncan/py-eodms-rapi',
     license='LICENSE',
-    description='A Python package for accessing the EODMS RAPI.',
+    description='EODMS RAPI Client is a Python3 package used to access the REST API service provided by the Earth Observation Data Management System (EODMS) from Natural Resources Canada.',
     long_description=open('README.md').read(),
     install_requires=[
         "dateparser", 
         "requests",
         "tqdm",
         "geomet",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
