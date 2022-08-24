11ec9dbce10039e38ae9070c25d9a3ae05cd72d4
Kevin Ballantyne <kevin.ballantyne@canada.ca>
2022-08-24 16:18:22 -0400

    Update to 1.5.1
    - Added optional timestamp to log output
    - Added the option to suppress tqdm progress
    
    	modified:   eodms_rapi/__init__.py
    	modified:   eodms_rapi/eodms.py
    	modified:   setup.cfg
    	modified:   setup.py
    	modified:   test/test_pyeodmsrapi.bat
    	modified:   test/test_pyeodmsrapi.py

--- test/test_pyeodmsrapi.bat
+++ test/test_pyeodmsrapi.bat
@@ -1,6 +1,3 @@
-SET EODMS_USER=kballan_test
-SET EODMS_PASSWORD=wtLotiD3#
-
 cd ..
 
 python setup.py install
@@ -11,6 +8,5 @@ python -m unittest test_pyeodmsrapi.TestEodmsRapi.test_search
 python -m unittest test_pyeodmsrapi.TestEodmsRapi.test_orderparameters
 python -m unittest test_pyeodmsrapi.TestEodmsRapi.test_deleteorder
 python -m unittest test_pyeodmsrapi.TestEodmsRapi.test_availablefields
-python -m unittest test_pyeodmsrapi.TestEodmsRapi.test_multiple_searches
 
 pause
