f5d52abd5a149309b17c912d8975086214cf539e
keballantyne <kevin.ballantyne@nrcan-rncan.gc.ca>
2025-02-24 16:29:31 -0500

    Fixed Submitting Orders to Include Start and End Dates
    	modified:   LICENSE
    	modified:   eodms_rapi/__init__.py
    	modified:   eodms_rapi/__version__.py
    	modified:   eodms_rapi/eodms.py
    	modified:   setup.cfg
    	modified:   setup.py

--- eodms_rapi/__init__.py
+++ eodms_rapi/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'py-eodms-rapi'
 __name__ = 'eodms_rapi'
 __author__ = 'Kevin Ballantyne'
 __copyright__ = 'Copyright (c) His Majesty the King in Right of Canada, ' \
-                'as represented by the Minister of Natural Resources, 2024'
+                'as represented by the Minister of Natural Resources, 2025'
 __license__ = 'MIT License'
 __description__ = 'A Python package to access the EODMS RAPI service.'
 __maintainer__ = 'Kevin Ballantyne'
